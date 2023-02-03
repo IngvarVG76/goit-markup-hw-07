5993e777df6f9ac2f135cf99dd9d92c885452d52
Igor Grybanov <igor.gribanov@gmail.com>
2023-02-03 19:54:40 +0200

    mistekes fixed

--- js/modal.js
+++ js/modal.js
@@ -10,5 +10,21 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+     }
+  
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[body-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("modal-shown");
   }
 })();
